ba87efa7af7be09800e4e907562cae8444e82452
zack <zcdorman@gmail.com>
2024-06-11 20:17:06 +0900

    files for testing asana stalker

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const githubInfo = require('./github-info');
 
 function run() {
-  githubInfo.getGithubInfo();
+  // githubInfo.getGithubInfo();
+
+  const eventBody = CustomGithub.getGithubEventsBody();
+  const eventUrl = CustomGithub.getEventUrl();
+  const hrefText = CustomGithub.getEventHrefText();
+  const linkList = UtilUrl.getAnyAsanaUrls(eventBody);
+  linkList.forEach((obj) => {
+      CustomAsana.getTaskNotesAndUpdateThemIfNeeded(obj.gid, eventUrl, hrefText);
+  })
 }
 
 run();
